5a1dfb4f1b53de3a2e2160882ca55f840979f618
vishallsinghh <vishalsingh173911@gmail.com>
2023-02-19 16:42:13 +0530

    sidebar_complete

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        "gray-900-spotify": "#121212",
+        "gray-800-spotify": "#181818",
+        "gray-700-spotify": "#282828",
+      },
+    },
+  },
+  plugins: [],
+};
